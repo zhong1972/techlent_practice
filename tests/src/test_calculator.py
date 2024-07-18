e65985791b93fb3466b857cebee6973ee82fd91f
zhong1972 <zhong1972@gmail.com>
2024-07-17 23:37:51 -0400

    add more

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -8,6 +8,5 @@ def test_subtract():
     assert cal.subtract(4,1)==3
 
     assert cal.subtract(2,1)==1
-def test_multiply():
-    assert cal.multiply(1,2)==2	
-    assert cal.multiply(2,3)==6
+    assert cal.subtract(3,1)==2	
+    assert cal.subtract(4,1)==3	
