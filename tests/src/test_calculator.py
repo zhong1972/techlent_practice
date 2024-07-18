c1c7d60ace2671937ac7b29cafc74287e3514eeb
zhong1972 <zhong1972@gmail.com>
2024-07-17 22:27:07 -0400

    add file

---
+++ tests/src/test_calculator.py
@@ -0,0 +1,12 @@
+from app.src.calculator import Calculator
+cal=Calculator()
+def test_add():
+    assert cal.add(1,1)==2
+    assert cal.add(1,2)==3
+def test_subtract():
+
+    assert cal.subtract(4,1)==3
+
+    assert cal.subtract(2,1)==1
+def test_multiply():
+    assert cal.multiply(1,2)==2	
